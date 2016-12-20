712dd76896fe662ecb2db3212f725bc77a973227
Esther Low <phi21416@gmail.com>
2016-12-20 10:17:24 +0800

    morning warm ups

---
+++ sumstring.js
@@ -0,0 +1,25 @@
+/*
+Sum String
+
+Write a function that takes as input a string that only contains numbers e.g. '1245'. Your function should take each individual character in the string and sum them together e.g. '1234' returns '13'
+Bonus
+
+Allow the string to include negative values e.g. '12-23' returns '4'
+*/
+
+
+function addString (stringNum) {
+var numArray = stringNum.split('')
+console.log(numArray);
+//var subtotal = 0
+
+//for (var i = 0; i < numArray.length; i++) {
+  //console.log(numArray[i]);
+
+ //  numArray[i] = parseInt(numArray[i], 10)
+ //  subtotal += numArray[i];
+ // }
+  //console.log(subtotal);
+}
+
+addString('12-34')
